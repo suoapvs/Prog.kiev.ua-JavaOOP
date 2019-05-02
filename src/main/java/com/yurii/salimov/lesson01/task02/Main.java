2321aba3db6ea93c1a40ac92eeb2dc4571bf401e
Yurii Salimov <yuriy.alex.salimov@gmail.com>
2019-05-02 10:15:39 +0300

    Added a comparison of non synchronized and synchronized counters.

--- src/main/java/com/yurii/salimov/lesson01/task02/Main.java
+++ src/main/java/com/yurii/salimov/lesson01/task02/Main.java
@@ -1,5 +1,8 @@
 package com.yurii.salimov.lesson01.task02;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 /**
  * 1.2 Написать класс, который умеет считать
  * количество созданных объектов этого класса
@@ -10,10 +13,59 @@ package com.yurii.salimov.lesson01.task02;
  */
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        singleThreadCounter();
+        multiThreadCounter();
+        multiThreadSynchronizedCounter();
+    }
+
+    private static void singleThreadCounter() {
+        Counter.reset();
         for (int i = 0; i < 100; i++) {
             new Counter();
         }
-        System.out.println(Counter.getCount());
+        System.out.println("Single thread counter: " + Counter.getCount());
+    }
+
+    private static void multiThreadCounter() throws InterruptedException {
+        Counter.reset();
+        final Collection<Thread> threads = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            final Thread th = new Thread(() -> {
+                try {
+                    Thread.sleep(System.nanoTime() % 50); // эмитируем бурную деятельность...
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                new Counter();
+            });
+            th.start();
+            threads.add(th);
+        }
+        for (Thread th : threads) {
+            th.join();
+        }
+        System.out.println("Multi thread counter: " + Counter.getCount());
+    }
+
+    private static void multiThreadSynchronizedCounter() throws InterruptedException {
+        SynchronizedCounter.reset();
+        final Collection<Thread> threads = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            final Thread th = new Thread(() -> {
+                try {
+                    Thread.sleep(System.nanoTime() % 50); // эмитируем бурную деятельность...
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                new SynchronizedCounter();
+            });
+            th.start();
+            threads.add(th);
+        }
+        for (Thread th : threads) {
+            th.join();
+        }
+        System.out.println("Multi thread synchronized counter: " + SynchronizedCounter.getCount());
     }
 }
