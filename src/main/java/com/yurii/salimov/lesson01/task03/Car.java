a043cf569343cd544d2a8e678f3b9dc2449dc6e6
Yurii Salimov <yuriy.alex.salimov@gmail.com>
2019-05-02 10:24:38 +0300

    Code refactoring.

--- src/main/java/com/yurii/salimov/lesson01/task03/Car.java
+++ src/main/java/com/yurii/salimov/lesson01/task03/Car.java
@@ -33,52 +33,53 @@ public final class Car {
     }
 
     public void speed(final int speed, final int acceleration) {
-        if (this.speed != speed) {
-            if (this.speed < speed) {
-                speedUp(speed, acceleration);
-            } else {
-                speedDown(speed, acceleration);
-            }
-            if (this.speed == 0) {
-                System.out.println("Car is stopped!");
-            }
+        if (!isStarted()) {
+            System.out.println("Start the engine!");
         } else {
-            System.out.println("Speed car is " + this.speed + " km/h now.");
+            if (this.speed != speed) {
+                if (this.speed < speed) {
+                    speedUp(speed, acceleration);
+                } else {
+                    speedDown(speed, acceleration);
+                }
+                if (this.speed == 0) {
+                    System.out.println("Car is stopped!");
+                }
+            } else {
+                System.out.println("Speed car is " + this.speed + " km/h now.");
+            }
         }
     }
 
     private void speedUp(final double speed, final double acceleration) {
-        if (!isStarted()) {
-            System.out.println("Start the engine!");
-        } else {
-            System.out.println("The car accelerates to " + speed + " km/h " +
-                    "with an acceleration of " + acceleration + " km/s:");
-            while (this.speed != speed) {
-                if (this.speed + acceleration > speed) {
-                    this.speed = speed;
-                }
-                System.out.println(this.speed + " km/h");
-                sleep();
+        System.out.println("The car accelerates to " + speed + " km/h " +
+                "with an acceleration of " + acceleration + " km/s:");
+        while (this.speed != speed) {
+            if (this.speed + acceleration > speed) {
+                this.speed = speed;
+            } else {
+                this.speed += acceleration;
             }
+            System.out.println(this.speed + " km/h");
+            sleep();
         }
     }
 
     private void speedDown(final int speed, final int acceleration) {
-        if (!isStarted()) {
-            System.out.println("Start the engine!");
-        } else {
-            System.out.println("The car slows to " + speed + " km/h " +
-                    "with an acceleration of -" + acceleration + " km/s:");
-            while (this.speed != speed) {
-                if (this.speed - acceleration < speed) {
-                    this.speed = speed;
-                }
-                System.out.println(this.speed + " km/h");
-                sleep();
+        System.out.println("The car slows to " + speed + " km/h " +
+                "with an acceleration of -" + acceleration + " km/s:");
+        while (this.speed != speed) {
+            if (this.speed - acceleration < speed) {
+                this.speed = speed;
+            } else {
+                this.speed -= acceleration;
             }
+            System.out.println(this.speed + " km/h");
+            sleep();
         }
     }
 
+    // Для эмитации набора скорости (разгона и торможения)...
     private void sleep() {
         try {
             Thread.sleep(500);
